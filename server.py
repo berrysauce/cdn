623482f62c24feea58eeddeb9a89c11953f351ba
Paul <paul@berrysauce.me>
2021-04-17 17:10:44 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from loguru import logger
+import sys
+from pydantic import BaseModel
+from typing import Optional
+import base64
+import random
+import string
+import json
+
+app = FastAPI()
+logger.add("api.log", format="[{time}] {level} - {message}", level="INFO", rotation="1 week", enqueue=True)
+
+class Item(BaseModel):
+    image: str
+
+@app.get("/image/{imgID}")
+async def get_image(imgID):
+    logger.info("Image request received & completed")
+    return FileResponse("images/"+imgID+".png")
+
+@app.post("/upload")
+async def upload(item: Item):
+    logger.info("Upload request received")
+    imgID = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 20))
+
+    with open("images/"+imgID+".png", "wb") as img:
+        img.write(base64.b64decode(item.image))
+    
+    logger.info("Upload request completed")
+    return imgID
+
+if __name__ == "__main__":
+    logger.info("Starting app")
+    uvicorn.run(app, host="127.0.0.1", port=8000)
