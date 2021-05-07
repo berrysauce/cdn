9a68f21742e5fb713607a78b930091df7efacae5
Paul <paul@berrysauce.me>
2021-05-07 17:50:39 +0200

    Add files via upload

--- brrycdn-cli/brrycdn_cli/main.py
+++ brrycdn-cli/brrycdn_cli/main.py
@@ -5,6 +5,7 @@ import base64
 import json
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import os
 
 app = typer.Typer()
 
@@ -13,12 +14,14 @@ def upload(host: str, username: str, password: str):
     typer.echo("Choose an image to upload")
     Tk().withdraw()
     image_path = askopenfilename()
+    root, extension = os.path.splitext(image_path)
     typer.secho("Uploading image...", fg=typer.colors.BRIGHT_BLUE)
     
     try:
         with open(image_path, "rb") as image_file:
             image = base64.b64encode(image_file.read())
-        data = json.dumps({"image":image.decode("utf-8")})
+        data = json.dumps({"image":image.decode("utf-8"),
+                           "file_type": extension})
         r = requests.post(host+"/upload", data=data, auth = HTTPBasicAuth(username, password))
     except:
         typer.secho("Error - failed to convert/upload image", fg=typer.colors.RED)
@@ -32,11 +35,11 @@ def upload(host: str, username: str, password: str):
 
 
 @app.command()
-def delete(image_id: str, host: str, username: str, password: str):
+def delete(image_id: str, file_type: str, host: str, username: str, password: str):
     typer.echo(f"Connecting to server...")
 
     try:
-        r = requests.post(host+"/delete/"+image_id, auth = HTTPBasicAuth(username, password))
+        r = requests.post(host+"/delete/"+image_id+"?file_type="+file_type, auth = HTTPBasicAuth(username, password))
         data = json.loads(r.text)
         msg = data["msg"]
         typer.secho(f"Got {r.status_code} - {msg}", fg=typer.colors.GREEN)
